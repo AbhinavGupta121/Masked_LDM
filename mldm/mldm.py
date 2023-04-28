fixes for mask loss normalization

mldm/mldm.py
@@ -26,6 +26,7 @@ from dataset import Custom_Val_Dataset
 from torch.utils.data import DataLoader
 from cldm.model import load_state_dict
 import matplotlib.pyplot as plt
+from dpmsolver import DPM_Solver
 
 def isfunction(object):
     """Return true if the object is a user-defined function.
@@ -367,10 +368,18 @@ class MaskControlLDM(ControlLDM):
             x0_pred_img = self.decode_first_stage(x0_pred)
             x0_gt = x0_gt.permute(0, 3, 1, 2)
             mask = mask.permute(0, 3, 1, 2)
+            # clip x_pred_img to[-1, 1]
+            x0_pred_img = torch.clamp(x0_pred_img, -1, 1)
             # print devices
             # print(x0_pred_img.device, x0_gt.device, mask.device)
             # print(x0_pred_img.shape, x0_gt.shape, mask.shape)
-            loss_mask = loss_fn.forward(x0_pred_img*mask, x0_gt*mask).mean()
+            # print max and min values of mask, gt and pred
+            # print("MAX MIN of pred", torch.max(x0_pred_img), torch.min(x0_pred_img), x0_pred_img.shape)
+            # print("MAX MIN of gt", torch.max(x0_gt), torch.min(x0_gt), x0_gt.shape)
+            # print("MAX MIN of mask", torch.max(mask), torch.min(mask), mask.shape)
+            # check if all elemts of mask are 0 or 1
+            # print("unique values of mask", torch.unique(mask))
+            loss_mask = loss_fn.forward(x0_pred_img*mask, x0_gt*mask).mean()*(x0_gt.shape[2]*x0_gt.shape[3]/torch.sum(mask))
             loss_dict.update({f'{prefix}/loss_mask': loss_mask})
             # loss = loss_mask
             loss = (1-self.mask_weight)*loss + self.mask_weight * loss_mask

train_mldm.py
@@ -8,6 +8,7 @@ from torch.utils.data import DataLoader
 from dataset import Custom_Train_Dataset, Custom_Val_Dataset, Custom_FID_Dataset
 from mldm.logger import ImageLogger
 from cldm.model import create_model, load_state_dict
+import numpy as np
 # TODO for loss:
 # - Code for 1 step prediction
 # - Mask is also there in the GT now, so multiply the loss by the mask
@@ -29,11 +30,16 @@ class CustomCheckpointCallback(pl.callbacks.ModelCheckpoint):
             super().on_batch_end(trainer, pl_module)
 
 def main():
+    # set global seed
+    pl.seed_everything(42)
+    # set numpy global seed
+    np.random.seed(42)
+
     # Configs
     resume_path = './models/control_sd15_openpose.pth' #start from openpose pretrained model
     batch_size = 1
     logger_freq = 300 # log images frequency
-    fid_logger_freq = 5000 # log fid frequency
+    fid_logger_freq = 30000 # log fid frequency
     loss_log_frequency = 300 # log loss frequency
     learning_rate = 1e-5
     sd_locked = True
@@ -42,7 +48,7 @@ def main():
     save_model_every_n_steps = 10000
     model_loss_type = 'mask'
     ddpm_mask_thresh = 200 # timestep below which mask loss is trained
-    mask_weight = 0.7 # loss = (1-mask weight)*sd_loss + mask_weight * mask_loss
+    mask_weight = 0.9 # loss = (1-mask weight)*sd_loss + mask_weight * mask_loss
 
     # First use cpu to load models. Pytorch Lightning will automatically move it to GPUs.
     model = create_model('./models/mldm_v15.yaml').cpu()
@@ -59,7 +65,7 @@ def main():
     # Misc
     logger = ImageLogger(batch_frequency=logger_freq, fid_frequency=fid_logger_freq, loss_log_frequency=loss_log_frequency, train_batch_size=batch_size)
     checkpointer= CustomCheckpointCallback(save_every_n_steps=save_model_every_n_steps)
-    trainer = pl.Trainer(gpus=[1], precision=32, callbacks=[logger, checkpointer])
+    trainer = pl.Trainer(gpus=[0], precision=32, callbacks=[logger, checkpointer])
     # can pass resume_from_checkpoint=resume_path to resume training
 
     train_dataloader = DataLoader(Custom_Train_Dataset(), num_workers=24, batch_size=batch_size, shuffle=True)

try_lpips.py
@@ -1,12 +1,97 @@
 import torch
+import lpips
+from PIL import Image
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
+
 _ = torch.manual_seed(123)
 from torchmetrics.image.lpip import LearnedPerceptualImagePatchSimilarity
-lpips = LearnedPerceptualImagePatchSimilarity(net_type='vgg').to("cuda:0")
+# lpips = LearnedPerceptualImagePatchSimilarity(net_type='vgg').to("cuda:0")
 # LPIPS needs the images to be in the [-1, 1] range.
-img1 = (torch.rand(10, 3, 100, 100) * 2) - 1
-img2 = (torch.rand(10, 3, 100, 100) * 2) - 1
-img1=img1.to("cuda:0")
-img2=img2.to("cuda:0")
-print(lpips(img1, img2))
 
-#print
+loss_fn = lpips.LPIPS(net='vgg',verbose=False).to("cuda:0")
+# loss_mask = loss_fn.forward(x0_pred_img*mask, x0_gt*mask).mean()
+
+img1 ="image_log/version217/train/loss/loss_gs-000000_e-000000_b-000000.png"
+img1 = Image.open(img1)
+img1 = np.array(img1)
+
+img1 = torch.from_numpy(img1).permute(2, 0, 1).unsqueeze(0).float()
+img1 /= 255.0
+
+noise_img1 = img1[:,:,2:514,2:514]
+predx0_img1 = img1[:,:,2:514,516:1028]
+gt_img1 = img1[:,:,2:514,1030:1542]
+mask_img1 = img1[:,:,2:514,1544:2056]
+
+noise_img1=noise_img1.to("cuda:0")
+gt_img1=gt_img1.to("cuda:0") # is in the range 0-1
+predx0_img1=predx0_img1.to("cuda:0")
+mask_img1=mask_img1.to("cuda:0")
+
+
+custom_mask1 = torch.zeros((512,512)).to("cuda:0")
+custom_mask1[0:256,:]=1
+num_pixels1 = torch.sum(custom_mask1)
+print("num_pixels1", num_pixels1)
+
+custom_mask2 = torch.zeros((512,512)).to("cuda:0")
+custom_mask2[0:25,:]=1
+num_pixels2 = torch.sum(custom_mask2)
+print("num_pixels2", num_pixels2)
+
+print("with mask1", loss_fn.forward(gt_img1*custom_mask1, predx0_img1*custom_mask1, retPerLayer=False, normalize=True).mean()*512*512/num_pixels1)
+print("with mask2", loss_fn.forward(gt_img1*custom_mask2, predx0_img1*custom_mask2, retPerLayer=False, normalize=True).mean()*512*512/num_pixels2)
+
+
+# mask_img1 = (mask_img1 > 0.5)
+# num_pixels = torch.sum(mask_img1)/3
+# print("number of pixels in mask out of 512*512:", num_pixels.sum())#58112
+
+
+
+# print("without mask")#0.3278
+# # print("same images ka lpips:", loss_fn.forward(gt_img1, gt_img1, retPerLayer=False, normalize=True).mean())
+# print("gt aur pred images ka lpips:", loss_fn.forward(gt_img1, predx0_img1, retPerLayer=False, normalize=True).mean())
+
+# print("with mask")#0.2576
+# # print("same images ka lpips:", loss_fn.forward(gt_img1*mask_img1, gt_img1*mask_img1, retPerLayer=False, normalize=True).mean()*512*512/num_pixels)
+# print("gt aur pred images ka lpips:", loss_fn.forward(gt_img1*mask_img1, predx0_img1*mask_img1, retPerLayer=False, normalize=True).mean()*512*512/num_pixels)
+
+
+# # to check if area change krne pe loss change ho rha hai ki nhi
+
+# print("100*100 vala area") #=0.2383
+# print("gt aur pred images ka lpips:", loss_fn.forward(gt_img1[:,:,250:350,250:350], predx0_img1[:,:,250:350,250:350], retPerLayer=False, normalize=True).mean())
+
+# print("150*150 vala area")#=0.2224
+# print("gt aur pred images ka lpips:", loss_fn.forward(gt_img1[:,:,225:375,225:375], predx0_img1[:,:,225:375,225:375], retPerLayer=False, normalize=True).mean())
+
+
+
+
+
+
+
+
+
+# print("same images ka lpips:", lpips(gt_img1, gt_img1))
+# print("gt aur pred images ka lpips:", lpips(gt_img1, predx0_img1))
+# print("gt aur mask images ka lpips:", lpips(gt_img1, mask_img1))
+
+# img1 = (torch.rand(1, 3, 100, 100) * 2) - 1
+# img2 = (torch.rand(1, 3, 100, 100) * 2) - 1
+# img1=img1.to("cuda:0")
+# img2=img2.to("cuda:0")
+# print(lpips(img1, img2))
+
+# loss_fn = lpips.LPIPS(net='vgg',verbose=False).to(x0_gt.device)
+# x0_pred = self.get_x0(x_noisy, t, cond, model_output)
+# x0_pred_img = self.decode_first_stage(x0_pred)
+# x0_gt = x0_gt.permute(0, 3, 1, 2)
+# mask = mask.permute(0, 3, 1, 2)
+# # print devices
+# # print(x0_pred_img.device, x0_gt.device, mask.device)
+# # print(x0_pred_img.shape, x0_gt.shape, mask.shape)
+# loss_mask = loss_fn.forward(x0_pred_img*mask, x0_gt*mask).mean()